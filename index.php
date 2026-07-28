20ab1b61b42f666c4bd9b60067e45e50b96a4132
Pl4inzz <mr.red208210@gmail.com>
2026-07-28 09:29:45 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'components/header.php'; ?>
+
+
+            <main>
+                <div class="container-fluid px-4 mt-3">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Kelas</th>
+                                <th>Nilai</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Jane Doe</td>
+                                <td>XII A</td>
+                                <td>96</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>John Doe</td>
+                                <td>XII A</td>
+                                <td>90</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+
+<?php include 'components/footer.php'; ?>
